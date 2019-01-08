1d728818e225e8ca2beb3017582e1a195646a28c
Aleksander Lukashou <lukashou@fev.com>
2019-01-08 11:40:02 +0100

    contrast-> sharpening -> denoising -> clahe

--- contrast.py
+++ contrast.py
@@ -10,14 +10,6 @@ def change_contrast(filepath, level):
     img = Image.open(filepath)
     img.load()
     #img = misc.img(gray=True).astype(float)
-    blurred_f = ndimage.gaussian_filter(img, 3)	
-    filter_blurred_f = ndimage.gaussian_filter(blurred_f, 1)
-    alpha = 30
-
-    sharpened = blurred_f + alpha * (blurred_f - filter_blurred_f)
-    im = ndimage.distance_transform_bf(sharpened)
-    im_noise = im + 0.2 * np.random.randn(*im.shape)
-    im_med = ndimage.median_filter(im_noise, 3)
 
     factor = (259 * (level+255)) / (255 * (259-level))
     for x in range(img.size[0]):
@@ -26,7 +18,17 @@ def change_contrast(filepath, level):
             new_color = tuple(int(factor * (c-128) + 128) for c in color)
             img.putpixel((x, y), new_color)
 
-    return img
+    
+    blurred_f = ndimage.gaussian_filter(img, 3)	
+    filter_blurred_f = ndimage.gaussian_filter(blurred_f, 1)
+    alpha = 20
+    sharpened = blurred_f + alpha * (blurred_f - filter_blurred_f)
+ 
+    im = ndimage.distance_transform_bf(sharpened)
+    im_noise = im + 0.2 * np.random.randn(*im.shape)
+    im_med = ndimage.median_filter(im_noise, 3)
+
+    return im_med
 
 def clahe (image_path):
 
@@ -42,10 +44,11 @@ def clahe (image_path):
 
 for filepath in glob.iglob('data/circle/*.jpg'):
 
+    result = change_contrast(filepath, 150)
+    #result.save(filepath)
+
     result = clahe(filepath)
     cv2.imwrite(filepath, result)
 
-    result = change_contrast(filepath, 180)
-    result.save(filepath)
 
 print('done')
